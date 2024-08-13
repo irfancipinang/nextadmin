c143f0a9df9e57c72b89a02f3799a672c20540c9
irfancipinang <muhammadirfanm.2006@gmail.com>
2024-08-13 12:12:05 +0700

    dashboard ui

---
+++ app/ui/dashboard/navbar/navbar.jsx
@@ -0,0 +1,9 @@
+const navbar = () => {
+  return (
+    <div>
+      Navbar
+    </div>
+  )
+}
+
+export default navbar
---
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -0,0 +1,9 @@
+const sidebar = () => {
+  return (
+    <div>
+      sidebar
+    </div>
+  )
+}
+
+export default sidebar
